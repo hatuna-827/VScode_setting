0a93f61d9c1ce845e402e98feed37553b24c6008
hatuna-827 <hatuna827yadaka@gmail.com>
2026-04-16 07:32:23 +0900

    add OneHalfDark

--- write.py
+++ write.py
@@ -19,30 +19,32 @@ def mix(color, rate, base=""):
         return rgb_to_hex(mixed)
 
 
-color_palette = {
-    "black-d": "#282C34",
-    "black-l": "#5A6374",
-    "red": "#E06C75",
-    "green": "#98C379",
-    "yellow": "#E5C07B",
-    "blue": "#61AFEF",
-    "purple": "#C678DD",
-    "cyan": "#56B6C2",
-    "white-d": "#DCDFE4",
-    "white-l": "#FFFFFF",
-}
+def generate_colors(accent_color):
+    color_palette = {
+        "black-d": "#282C34",
+        "black-l": "#5A6374",
+        "blue": "#61AFEF",
+        "cyan": "#56B6C2",
+        "green": "#98C379",
+        "yellow": "#E5C07B",
+        "orange": "#E29678",
+        "red": "#E06C75",
+        "purple": "#C678DD",
+        "white-d": "#DCDFE4",
+        "white-l": "#FFFFFF",
+    }
 
-background = color_palette["black-d"]
-background_ = color_palette["black-l"]
-foreground = color_palette["white-d"]
-foreground_ = color_palette["white-l"]
-accent = color_palette["red"]
-error = color_palette["red"]
-warning = color_palette["yellow"]
+    color_list = ["yellow", "purple", "blue", "orange", "green", "red"]
 
-settings = f"""{{
-  //"workbench.colorTheme": "Visual Studio Light",
-  "workbench.colorCustomizations": {{
+    background = color_palette["black-d"]
+    background_ = color_palette["black-l"]
+    foreground = color_palette["white-d"]
+    foreground_ = color_palette["white-l"]
+    accent = color_palette[accent_color]
+    error = color_palette["red"]
+    warning = color_palette["yellow"]
+
+    colors = f"""
     "activityBar.background": "{background}",
     "activityBar.activeBorder": "{accent}",
     "activityBar.border": "{background_}",
@@ -51,8 +53,8 @@ settings = f"""{{
     "activityBarBadge.background": "{accent}",
     "activityBarBadge.foreground": "{background}",
     "button.foreground": "{background}",
-    "button.background": "{accent}",
-    "button.hoverBackground": "{foreground}",
+    "button.background": "{mix(accent,.7,background_)}",
+    "button.hoverBackground": "{mix(accent,.9,background_)}",
     "editor.background": "{background}",
     "editor.foreground": "{foreground}",
     "editor.selectionBackground": "{mix(accent,.3)}",
@@ -62,12 +64,13 @@ settings = f"""{{
     "editorLineNumber.activeForeground": "{accent}",
     "editorBracketMatch.background": "{mix(accent,.25)}",
     "editorBracketMatch.border": "{mix(accent,.25)}",
+    "editorLink.activeForeground": "{accent}",
     "editorError.foreground": "{error}",
     "editorWarning.foreground": "{warning}",
     "focusBorder": "{accent}",
     "menu.background": "{background}",
     "menu.foreground": "{foreground}",
-    "menu.border":"{foreground}",
+    "menu.border":"{mix(accent,.2,background)}",
     "menu.selectionBackground": "{accent}",
     "menu.selectionForeground": "{foreground}",
     "menu.separatorBackground": "{foreground}",
@@ -98,6 +101,8 @@ settings = f"""{{
     "tab.unfocusedActiveBackground": "{mix(accent,.2)}",
     "tab.unfocusedInactiveBackground": "{mix(accent,.1)}",
     "tab.unfocusedHoverBackground": "{mix(accent,.3)}",
+    "textLink.foreground": "{mix(accent,.6,foreground)}",
+    "textLink.activeForeground": "{accent}",
     "editorGroupHeader.tabsBackground": "{background}",
     "terminal.background": "{background}",
     "terminal.foreground": "{foreground}",
@@ -108,19 +113,70 @@ settings = f"""{{
     "terminalCursor.foreground": "{accent}",
     "progressBar.background": "{accent}",
     "titleBar.activeBackground": "{background}",
-    "titleBar.border": "{background}"
-  }},
-  "editor.tokenColorCustomizations": {{
-    "comments": "{background_}",
-    "strings": "{color_palette['red']}",
-    "numbers": "{color_palette['purple']}",
-    "keywords": "{color_palette['blue']}",
-    "functions": "{color_palette['green']}",
-    "types": "{color_palette['yellow']}",
-    "variables": "{color_palette['cyan']}"
-  }}
+    "titleBar.border": "{background}",
+    "editorBracketHighlight.foreground1": "{color_palette[color_list[0]]}",
+    "editorBracketHighlight.foreground2": "{color_palette[color_list[1]]}",
+    "editorBracketHighlight.foreground3": "{color_palette[color_list[2]]}",
+    "editorBracketHighlight.foreground4": "{color_palette[color_list[3]]}",
+    "editorBracketHighlight.foreground5": "{color_palette[color_list[4]]}",
+    "editorBracketHighlight.foreground6": "{color_palette[color_list[5]]}",
+    "diffEditor.insertedTextBackground": "{mix(color_palette['green'],.15)}",
+    "diffEditor.removedTextBackground": "{mix(color_palette['red'],.15)}",
+    "editorGutter.addedBackground": "{color_palette['green']}",
+    "editorGutter.deletedBackground": "{color_palette['red']}",
+    "editorGutter.modifiedBackground": "{color_palette['blue']}",
+    "diffEditor.insertedLineBackground": "{mix(color_palette['green'],.1)}",
+    "diffEditor.removedLineBackground": "{mix(color_palette['green'],.1)}",
+    "quickInput.background": "{background}",
+    "quickInput.foreground": "{foreground}",
+    "quickInput.border": "{mix(accent,.1,background)}",
+    "quickInputList.focusBackground": "{mix(accent,.2,background)}",
+    "quickInputList.focusForeground": "{foreground}",
+"""
+
+    tokenColors = f"""
+    {{ "scope": "comment", "settings": {{ "foreground": "{background_}" }} }},
+    {{ "scope": "string", "settings": {{ "foreground": "{color_palette['red']}" }} }},
+    {{ "scope": "constant.numeric", "settings": {{ "foreground": "{color_palette['purple']}" }} }},
+    {{ "scope": "keyword", "settings": {{ "foreground": "{color_palette['blue']}" }} }},
+    {{ "scope": "entity.name.function", "settings": {{ "foreground": "{color_palette['green']}" }} }},
+    {{ "scope": "storage.type", "settings": {{ "foreground": "{color_palette['yellow']}" }} }},
+    {{ "scope": "variable", "settings": {{ "foreground": "{color_palette['cyan']}" }} }}
+"""
+
+    settings = f"""{{
+    "workbench.colorCustomizations": {{{colors}  }},
+    "editor.tokenColorCustomizations": {{
+        "textMateRules": [{tokenColors}    ]
+    }}
+    }}
+"""
+
+    themes = f"""{{
+  "colors": {{{colors}  }},
+  "tokenColors": [{tokenColors}  ]
 }}
 """
 
-with open("./.vscode/settings.json", "w") as o:
-    o.write(settings)
+    return (settings, themes)
+
+
+accent_colors = [
+    ("black-l", "Black"),
+    ("blue", "Blue"),
+    ("cyan", "Cyan"),
+    ("green", "Green"),
+    ("yellow", "Yellow"),
+    ("red", "Red"),
+    ("orange", "Orange"),
+    ("purple", "Purple"),
+]
+
+# (settings, themes) = generate_colors("green")
+# with open("./.vscode/settings.json", "w") as o:
+#     o.write(settings)
+
+for theme_name, file_name in accent_colors:
+    (settings, themes) = generate_colors(theme_name)
+    with open(f"./colorthemes/OneHalfDark/{file_name}.json", "w") as o:
+        o.write(themes)
